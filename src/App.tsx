c387377aaaa566f36aaf57dbf8b772e5f06ec28b
Kelvin <kelvinperosa.kp@gmail.com>
2026-07-10 17:47:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import Header from './components/Header/Header.tsx'
+import Hero from './components/Hero/Hero.tsx'
+import Countdown from './components/Countdown/Countdown.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Countdown />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import './Header.css'
+import {FiHeart} from 'react-icons/fi'
+
+function Header() {
+    return(
+        <header className="header">
+            <div className="logo">
+                <FiHeart/>
+            </div>
+
+            <nav className="menu">
+                <a href="#">Inicio</a>
+                <a href="#">Presentes</a>
+            </nav>
+
+            <button className="btn-confirmar">
+                Confirmar Presença
+            </button>
+            
+        </header>
+    )
+}
+
+export default Header
